1169661269dd3fc421b5751d27bdf5ff58ec7977
jihadhassan <jihadhussein8@gmail.com>
2019-06-17 12:01:26 +0200

    add resturant/hotel page with all nested components

---
+++ src/app/feature/restaurant/restaurant.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Resturant } from 'src/app/_models/resturant';
+import { ResturantService } from './resturant.service';
+
+@Component({
+  selector: 'restaurant',
+  templateUrl: './restaurant.component.html',
+  styleUrls: ['./restaurant.component.scss']
+})
+export class RestaurantComponent implements OnInit {
+@Input() resturant:Resturant;
+  constructor(private resturantt:ResturantService) { }
+
+  ngOnInit() {
+    this.resturant=this.resturantt.getResturantById()
+    console.log(this.resturant)
+  }
+
+}
